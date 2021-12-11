f1f1dcb460e42075fde871793e38202c412127b3
kimchan122 <chanchankim122@gmail.com>
2021-12-12 00:20:44 +0900

    로그인 화면 구현

--- app/src/main/java/com/example/bookmoa/MainActivity.java
+++ app/src/main/java/com/example/bookmoa/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bookmoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //이하는 단순히 넘어가는 것을 확인하기 위한 것으로, 추후 삭제해도 무방
+        Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);
     }
     //test
 }
